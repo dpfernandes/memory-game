da78aaabe4530ecdc44ee00a1acdf48858054427
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2020-07-22 19:15:19 +0200

    deletion of old components that are not being used

--- src/client/components/LocationComponent/LocationComponent.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import LocationComponent from './LocationComponent';
-
-export default { title: 'LocationComponent' };
-
-export const LocationComponents = () => <LocationComponent />;
--- src/client/components/Map/Map.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Mymap from './Map';
-
-import './Map.css';
-
-export default { title: 'Map' };
-
-export const Map = () => <Mymap latitude="55.661869" longitude="12.540742" />;
