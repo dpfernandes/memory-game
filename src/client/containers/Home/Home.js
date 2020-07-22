644dbf478626c828346523974c977f618c9d57a0
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2020-07-22 19:42:05 +0200

    changes on Home with the new components

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import '../../App.css';
 
-// import GameLogoComponent from '../../components/GameLogoComponent/GameLogoComponent';
-// import SpoonImageComponent from '../../components/SpoonImageComponent/SpoonImageComponent';
-// import ForkImageComponent from '../../components/ForkImageComponent/ForkImageComponent';
+import GameLogoComponent from '../../components/GameLogoComponent/GameLogoComponent';
+import SpoonImageComponent from '../../components/SpoonImageComponent/SpoonImageComponent';
+import ForkImageComponent from '../../components/ForkImageComponent/ForkImageComponent';
 import Title from '../../components/Title/Title';
-// import SubmitButton from '../../components/SubmitButton/SubmitButton';
-// import Footer from '../../components/FooterComponent/FooterComponent';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import Footer from '../../components/FooterComponent/FooterComponent';
 
 export const Home = () => (
   <section className="home bodycontainer">
     <div className="bodywrapper">
       <div className="headercontainer">
-        <div className="homelogo">{/* <GameLogoComponent /> */}</div>
+        <div className="homelogo">
+          <GameLogoComponent />
+        </div>
         <Title title="Memory Game" subtitle="- perserve culinary heritage -" />
       </div>
-      {/* <ForkImageComponent /> */}
-      {/* <SubmitButton
+      <ForkImageComponent />
+      <SubmitButton
         buttonTitle="Play EAT THE PAIR"
         onClick={() => {
           location.href = '../pick-game-page';
         }}
-      /> */}
-      {/* <SpoonImageComponent />  */}
-      {/* <Footer /> */}
+      />
+      <SpoonImageComponent />
+      <Footer />
     </div>
   </section>
 );
