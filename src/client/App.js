f68a4cf5362d33f5aec945b24cc3b7e3584e8abc
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2020-07-22 18:19:49 +0200

    app.js route changes

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import { Home } from './containers/Home/Home';
+import { ErrorPage } from './containers/404Page/404Page.component';
+import { AppContext } from './AppContext';
 
 function App() {
+  const [appState, setAppState] = useState({ gameCode: '20', teamName: '' });
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <AppContext.Provider value={{ appState, setAppState }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/404-page">
+            <ErrorPage />
+          </Route>
+          <Redirect to="/404-page" />
+        </Switch>
+      </Router>
+    </AppContext.Provider>
   );
 }
 export default App;
