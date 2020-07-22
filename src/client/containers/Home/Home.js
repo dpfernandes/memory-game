0d181aedab1c9a8dd524911c20571896ceea1d3e
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2020-07-22 18:36:20 +0200

    changes on Home.js

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -1,3 +1,29 @@
 import React from 'react';
+import '../../App.css';
 
-export const Home = () => <section className="home">Final project!</section>;
+// import GameLogoComponent from '../../components/GameLogoComponent/GameLogoComponent';
+// import SpoonImageComponent from '../../components/SpoonImageComponent/SpoonImageComponent';
+// import ForkImageComponent from '../../components/ForkImageComponent/ForkImageComponent';
+import Title from '../../components/Title/Title';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import Footer from '../../components/FooterComponent/FooterComponent';
+
+export const Home = () => (
+  <section className="home bodycontainer">
+    <div className="bodywrapper">
+      <div className="headercontainer">
+        <div className="homelogo">{/* <GameLogoComponent /> */}</div>
+        <Title title="Memory Game" subtitle="- perserve culinary heritage -" />
+      </div>
+      {/* <ForkImageComponent /> */}
+      <SubmitButton
+        buttonTitle="Play EAT THE PAIR"
+        onClick={() => {
+          location.href = '../pick-game-page';
+        }}
+      />
+      {/* <SpoonImageComponent />  */}
+      <Footer />
+    </div>
+  </section>
+);
